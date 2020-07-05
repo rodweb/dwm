89094df80083fea1393f6df7c8d2021d4cfd86df
Rodrigo Campos <rod.apd@gmail.com>
2020-07-05 18:19:59 -0300

    Add swallow patch

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -26,12 +27,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
-	{ "Spotify",  NULL,       NULL,       1 << 4,       0,           0,           -1 },
-	{ "Slack",  NULL,       NULL,       1 << 3,       0,           0,           -1 },
-	{ "Google-chrome-unstable",  NULL,       NULL,       1 << 0,       0,           0,           -1 },
+	/* class        instance    title               tags mask     iscentered   isfloating   monitor   isterminal   noswallow  ispermanent*/
+	{ "Spotify",  NULL,       NULL,                 1 << 6,         0,           0,           -1,           0,      -1,     0 },
+	{ "Slack",  NULL,       NULL,                   1 << 5,         0,           0,           -1,           0,      -1,     0 },
+	{ "Google-chrome-unstable",  NULL,       NULL,  1 << 0,         0,           0,           -1,           0,      -1,     0 },
+	{ "st-256color",         NULL,       NULL,      0,              0,           0,           -1,           1,      -1,     0 },
+	{ "Alacritty",         NULL,       NULL,        0,              0,           0,           -1,           1,      -1,     0 },
+	{ NULL,         NULL,       "Event Tester",     0,              0,           0,           -1,           0,      1,      0 }, /* xev */
 };
 
 /* layout(s) */

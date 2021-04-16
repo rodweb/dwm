0391f562efe6d79d0589011f0f456e629cff29eb
Rodrigo Campos <rod.apd@gmail.com>
2021-04-16 09:39:17 -0300

    Change tags to asdf

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#665e5e";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "tmux", "chat", "code", "web" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,10 +28,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class        instance    title               tags mask     iscentered   isfloating   monitor   isterminal   noswallow  ispermanent*/
-	{ "Spotify",    NULL,       NULL,                 1 << 4,         0,           0,           -1,           0,      -1,     0 },
-	{ "Slack",      NULL,       NULL,                 1 << 3,         0,           0,           -1,           0,      -1,     0 },
-	{ "TelegramDesktop",  NULL,       NULL,           1 << 3,         0,           0,           -1,           0,      -1,     0 },
-	{ "Google-chrome-stable",  NULL,       NULL,      1 << 0,         0,           0,           -1,           0,      -1,     0 },
+	{ "Spotify",    NULL,       NULL,                 1 << 2,         0,           0,           -1,           0,      -1,     0 },
+	{ "Slack",      NULL,       NULL,                 1 << 1,         0,           0,           -1,           0,      -1,     0 },
+	{ "TelegramDesktop",  NULL,       NULL,           1 << 1,         0,           0,           -1,           0,      -1,     0 },
+	{ "Google-chrome-stable",  NULL,       NULL,      1 << 3,         0,           0,           -1,           0,      -1,     0 },
 	{ "st-256color",         NULL,       NULL,      0,              0,           0,           -1,           1,      -1,     0 },
 	{ "Alacritty",         NULL,       NULL,        0,              0,           0,           -1,           1,      -1,     0 },
 	{ NULL,         NULL,       "Event Tester",     0,              0,           0,           -1,           0,      1,      0 }, /* xev */
@@ -75,8 +75,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_o,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
@@ -89,23 +89,18 @@ static Key keys[] = {
 	/* { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, */
 	/* { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, */
 	/* { MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} }, */
-	{ MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
-	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,             XK_g,      togglefloating, {0} },
+	{ MODKEY,                       XK_g,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_a,                      0)
+	TAGKEYS(                        XK_s,                      1)
+	TAGKEYS(                        XK_d,                      2)
+	TAGKEYS(                        XK_f,                      3)
 	{ MODKEY,                       XK_z,      quit,           {0} },
 	{ MODKEY,                       XK_u,      focusurgent,    {0} },
 };

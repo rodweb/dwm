5f33dcd3e32dc03d91f6e82418e251a287700082
Rodrigo Campos <rod.apd@gmail.com>
2023-07-01 15:51:23 -0300

    Open apps on stack

--- dwm.c
+++ dwm.c
@@ -1281,8 +1281,8 @@ manage(Window w, XWindowAttributes *wa)
 		c->isfloating = c->oldstate = trans != None || c->isfixed;
 	if (c->isfloating)
 		XRaiseWindow(dpy, c->win);
-	/* attachbottom(c); */
-	attach(c);
+	attachbottom(c);
+	/* attach(c); */
 	attachstack(c);
 	XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32, PropModeAppend,
 		(unsigned char *) &(c->win), 1);
